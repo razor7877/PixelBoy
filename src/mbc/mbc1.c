5114b132c2329678b15f8f8fcd2ad1dbc943f700
razor7877 <razor7877@gmail.com>
2024-03-30 21:34:51 +0100

    Forgot to include mbc1 update in last commit #1

--- src/mbc/mbc1.c
+++ src/mbc/mbc1.c
@@ -20,10 +20,14 @@ uint8_t read_rom_mbc1(uint16_t address)
     // Read RAM banks 00-03
     if (address >= 0xA000 && address <= 0xBFFF)
     {
+        log_debug("External RAM read ADR %x - ram_enable is %x\n", address, mbc1.ram_enable);
+        // Read RAM banks 00-03
         if (!mbc1.ram_enable)
             return 0xFF;
 
+        uint16_t mapped_address = (mbc1.ram_bank << 12) | (address & 0x1FFF);
 
+        return external_ram[mapped_address];
     }
 
 	return 0xFF;
@@ -33,7 +37,7 @@ void write_rom_mbc1(uint16_t address, uint8_t value)
 {
     if (address >= 0x0000 && address <= 0x1FFF)
     {
-        log_debug("Update RAM enable register\n");
+        //log_debug("Update RAM enable register\n");
         if ((value & 0x0F) == 0x0A)
             mbc1.ram_enable = true;
         else
@@ -47,14 +51,14 @@ void write_rom_mbc1(uint16_t address, uint8_t value)
         if (mbc1.rom_bank == 0) // 0 value behaves as a 1 on real hardware
             mbc1.rom_bank = 1;
 
-        log_debug("Update ROM bank number - Value: %d\n", mbc1.rom_bank);
+        //log_debug("Update ROM bank number - Value: %d\n", mbc1.rom_bank);
     }
 
     // 2 bit RAM bank number register 
     if (address >= 0x4000 && address <= 0x5FFF)
     {
         mbc1.ram_bank = value & 0x03; // 2 lower bits mask
-        log_debug("Update RAM bank number - Value: %d\n", mbc1.ram_bank);
+        //log_debug("Update RAM bank number - Value: %d\n", mbc1.ram_bank);
     }
 
     // 1 bit banking mode register
@@ -62,6 +66,21 @@ void write_rom_mbc1(uint16_t address, uint8_t value)
     {
         mbc1.banking_mode = value & 0b1;
         //log_debug("Update banking mode select - Value: %x\n", mbc1.banking_mode);
+    }
+
+    if (address >= 0xA000 && address <= 0xBFFF)
+    {
+        if (!mbc1.ram_enable)
+            return;
+
+        if (mbc1.banking_mode == 1)
+        {
+            log_debug("Bank mode 1 RAM write\n");
+        }
+
+        uint16_t mapped_address = (mbc1.ram_bank << 12) | (address & 0x1FFF);
+        external_ram[mapped_address] = value;
+        log_debug("External RAM write ADR %x VALUE %x\n", address, value);
     }
 }
 
